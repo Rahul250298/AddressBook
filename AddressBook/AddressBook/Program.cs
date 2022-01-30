08ad96e4923b83a20b5de3ab5b0d8efd2942a064
rahul <rahulravi2502@gmail.com>
2022-01-30 20:36:12 +0530

    Addinmg Name Editing

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -11,19 +11,10 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Addressd Book Program ");
-            Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
-            CreateAddressBook addressBook = new CreateAddressBook()  //Initializing elements using collection-initializer syntax
-            {
-                FirstName = Console.ReadLine(),
-                LastName = Console.ReadLine(),
-                Address = Console.ReadLine(),
-                City = Console.ReadLine(),
-                State = Console.ReadLine(),
-                Zip = Convert.ToDouble(Console.ReadLine()),
-                PhoneNumber = Convert.ToDouble(Console.ReadLine()),
-                Email = Console.ReadLine(),
-            };
-            addressBook.displayContact();
+            Console.WriteLine("Welcome to Address Book Progam \n");
+            CreateAddressBook createAddressBook = new CreateAddressBook();
+            createAddressBook.ReadInput();
+            Console.ReadLine();
             Console.ReadLine();
         }
     }
