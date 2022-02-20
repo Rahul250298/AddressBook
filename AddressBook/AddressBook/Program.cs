5de0d79127ad2f4b60f16d108398800504b2e31b
rahul <rahulravi2502@gmail.com>
2022-02-20 16:07:17 +0530

    No Duplicate Person

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -11,7 +11,6 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Addressd Book Program ");
-            Console.WriteLine("Welcome to Address Book Progam \n");
             CreateAddressBook createAddressBook = new CreateAddressBook();
             createAddressBook.ReadInput();
             Console.ReadLine();
